a6667f748b6d2600791c6b7447a23f6bad7e66e1
Max Ogden <max@maxogden.com>
2014-11-04 15:09:57 +0000

    add .createFunctionStream export

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var pumpify = require('pumpify')
 var through = require('through2')
 
 module.exports = function(func) {
-  if (typeof func === 'string') {
-    var funcStr = func + ';\n return this;'
-    if (func[0] === '{') funcStr = 'var that = ' + func + ';\n return that;'
-    var compiled = new Function(funcStr)
-  } else {
-    compiled = func
-  }
-  
-  return pumpify.obj(ldj.parse(), through.obj(transform))
+  var transform = createFunctionStream(func)
+  return pumpify.obj(ldj.parse(), transform) 
+}
+
+module.exports.createFunctionStream = createFunctionStream
+
+function createFunctionStream(func) {
+  var funcStr = func + ';\n return this;'
+  if (func[0] === '{') funcStr = 'var that = ' + func + ';\n return that;'
+  var compiled = new Function(funcStr)
   
   function transform(obj, enc, next) {
     var out = compiled.call(obj, obj)
     this.push(out)
     next()
   }
+
+  return through.obj(transform)
 }
