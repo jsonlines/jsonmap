0086205fa72a2016147a6cd9a41cb621f43b9fac
Mathias Buus <mathiasbuus@gmail.com>
2014-12-16 14:52:59 +0100

    moved tests to their own folder

--- test/index.js
+++ test/index.js
@@ -1,12 +1,14 @@
 var exec = require('child_process').exec;
 
-exec('cat test.json | node cli.js \"{dog: this.dog}\"',
+process.chdir(__dirname)
+
+exec('cat test.json | node ../cli.js \"{dog: this.dog}\"',
   expect('{"dog":5}\n{"dog":6}\n'));
 
-exec('cat test.json | node cli.js --file=test-transform.js',
+exec('cat test.json | node ../cli.js --file=test-transform.js',
   expect('{"summary":["foo","dog"]}\n{"summary":["baz","dog"]}\n'));
 
-exec('cat test.json | node cli.js \"{dog: this.dog}\" | node cli.js --file=test-transform2.js',
+exec('cat test.json | node ../cli.js \"{dog: this.dog}\" | node ../cli.js --file=test-transform2.js',
   expect('{"dog":6}\n{"dog":7}\n'));
 
 function expect(output) {
