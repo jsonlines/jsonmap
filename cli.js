74f3a45665ab0a76e4eec7e4457ca919956d0de8
Max Ogden <max@maxogden.com>
2015-05-06 10:02:09 -0700

    add cli usage

--- cli.js
+++ cli.js
@@ -4,9 +4,17 @@ var map = require('./')
 var ndjson = require('ndjson')
 var args = require('minimist')(process.argv.slice(2))
 var path = require('path')
+var fs = require('fs')
+
+var usage = fs.readFileSync('./usage.txt').toString()
 
 var transform = args._[0]
 if (args.file) transform = require(path.resolve(process.cwd(), args.file))
+
+if (!transform) {
+  console.error(usage)
+  process.exit(1)
+}
 
 process.stdin
   .pipe(map(transform, args))
